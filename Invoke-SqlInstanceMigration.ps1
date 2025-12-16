de3bfdbf9807aaf59c86998652779e98230a3dcc
karim-attaleb <41701245+karim-attaleb@users.noreply.github.com>
2025-12-16 09:41:42 +0100

    proper [DbaInstanceParameter] datatype
    proper [DbaInstanceParameter] datatype

--- Invoke-SqlInstanceMigration.ps1
+++ Invoke-SqlInstanceMigration.ps1
@@ -2,18 +2,21 @@
 .SYNOPSIS
     CLI entry point for SqlInstanceMigrator.
 #>
-[CmdletBinding()]
+#[CmdletBinding()]
 param(
     [Parameter(Mandatory)][string]$SourceInstance,
     [Parameter(Mandatory)][string]$TargetInstance,
     [string]$SharedPath,
     [string]$ConfigPath,
     [string]$LogPath
-    
 )
 
 $modulePath = Join-Path $PSScriptRoot "Modules\SqlInstanceMigrator.psd1"
 Import-Module $modulePath -Force
+
+[DbaInstanceParameter]$SourceInstance = Connect-DbaInstance $SourceInstance
+[DbaInstanceParameter]$TargetInstance = Connect-DbaInstance $TargetInstance
+
 
 $Params = @{
     SourceInstance = $SourceInstance
@@ -22,6 +25,5 @@ $Params = @{
 if ($SharedPath) { $Params.SharedPath = $SharedPath }
 if ($ConfigPath) { $Params.ConfigPath = $ConfigPath }
 if ($LogPath) { $Params.LogPath = $LogPath }
-#if ($WhatIf) { $Params.WhatIf = $true }
 
 Start-SqlInstanceMigration @Params
